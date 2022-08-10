191372bccfb0030a1ee5a090194e82247a0902fc
rutvik-sr <rutvik.softrefine@gmail.com>
2022-08-10 18:19:46 +0530

    google authentication

---
+++ lib/services/users.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+late String uname;
+late String email;
+late String imageUrl;
+
+Future<String> signInWithGoogle() async {
+  final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount!.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final UserCredential authResult = await auth.signInWithCredential(credential);
+  final User? user = authResult.user;
+
+  assert (user?.email != null);
+  assert (user?.displayName != null);
+  assert (user?.photoURL != null);
+
+  uname = user!.displayName!;
+  email = user.email!;
+  imageUrl = user.photoURL!;
+
+  final User currentUser = auth.currentUser!;
+  assert(user.uid == currentUser.uid);
+
+  return 'signInWithGoogle succeeded: $user';
+}
+
+  void signOutGoogle() async {
+  await googleSignIn.signOut();
+  
+  print('User Sign Out');
+  }
